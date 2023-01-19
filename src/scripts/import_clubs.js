bf19e5b4a20471878df304134fe279d5b31aba22
noelle <noelle.mtf@gmail.com>
2023-01-19 12:28:40 -0300

    fix: CSS e javaScript

--- src/scripts/import_clubs.js
+++ src/scripts/import_clubs.js
@@ -1,3 +1,5 @@
 import { getClubs } from "./clubs_requests.js";
 
-import {renderCards, createCards} from './clubs.js'
+import {renderCards, createCards, changeBetweenPages} from './clubs.js'
+
+changeBetweenPages()
